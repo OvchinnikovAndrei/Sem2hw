152c89b6609e32f65a08c27fe2a8683ee29ea36c
Andrey Ovchinnikov <sunnyhand@bk.ru>
2022-12-04 00:31:51 +0300

    Задача 13 отрицательное число

--- task13/Program.cs
+++ task13/Program.cs
@@ -4,35 +4,35 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-Console.WriteLine("Введи число: ");
-int num = int.Parse(Console.ReadLine()!);
-string numText = Convert.ToString(num);
-
-if (numText.Length > 2)
-{
-    Console.WriteLine(numText[2]);
-}
-else
-{
-    Console.WriteLine("Третьей цифры нет");
-}
-
-
-
-
 // Console.WriteLine("Введи число: ");
 // int num = int.Parse(Console.ReadLine()!);
 // string numText = Convert.ToString(num);
 
-// if (numText.Length > 2 || num > 0)
+// if (numText.Length > 2)
 // {
 //     Console.WriteLine(numText[2]);
 // }
-// else if (numText.Length < 3)
+// else
 // {
 //     Console.WriteLine("Третьей цифры нет");
 // }
-// else
-// {
-//     Console.WriteLine(numText[3]);
-// }
+
+
+
+
+Console.WriteLine("Введи число: ");
+int num = int.Parse(Console.ReadLine()!);
+string numText = Convert.ToString(num);
+
+if (numText.Length > 2 && num > 0)
+{
+    Console.WriteLine(numText[2]);
+}
+else if (numText.Length < 3 || (numText.Length < 4 && num < 0))
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine(numText[3]);
+}
